b89ee1360d9133da58feb1dd7080becaf7f293da
David Sommer <30873760+sommerda@users.noreply.github.com>
2018-10-09 03:11:59 +0200

    moved style attributes to css file

--- privacy_webpage/webpage/bucketserver/static/graphs.js
+++ privacy_webpage/webpage/bucketserver/static/graphs.js
@@ -1,20 +1,14 @@
-function append_plotly_plot_dict(container, plot_dict){
-	var div = document.createElement("div");
-    div.style.display = "block";
-    div.style.width = "700px";
-    div.style.height = "320px";
-    container.appendChild(div);
-
-    Plotly.plot(div, plot_dict['traces'], plot_dict['layout']);
-}
-
 // this function deploys plotly plots in the given container
-function create_plotly_plots(container, plots_dict_string){
-	plots_dict = JSON.parse(plots_dict_string);
-	// The data dict should be ordered
-	for (const [key, value] of Object.entries(plots_dict)) {
-  		console.log("elaborate on plot-key: " + key);
-  		append_plotly_plot_dict(container, value);
+function create_plotly_plots(container, plot_dicts_json){
+	plot_dicts = JSON.parse(plot_dicts_json);
+	
+	for (const [key, plot_dict] of Object.entries(plot_dicts)) {
+
+  		var div = document.createElement("div");
+		div.classList.add("plotlyplot");
+    	container.appendChild(div);
+
+    	Plotly.plot(div, plot_dict['traces'], plot_dict['layout']);
 	}
 }
 
@@ -22,8 +16,7 @@ function create_plotly_plots(container, plots_dict_string){
 // this functions displays a base64 encoded image in the given container
 function create_base64string_plot(container, string){
 	var img = document.createElement("img");
-    img.style.display = "block";
-    img.style.width = "500px";
+	img.classList.add("base64stringplot");
     img.src = string;
     image_container.appendChild(img);
 }
